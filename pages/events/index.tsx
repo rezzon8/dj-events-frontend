45933efa5948055dfdbedb742d19f38de9eea503
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2023-10-10 22:24:45 +0200

    Refactor

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -3,22 +3,22 @@ import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
 import { DjEvents, DjEvent } from '@/types/dj-events'
 
-export async function getServerSideProps() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
-  return {
-    props: { events }
-  }
-}
-
 export default function EventsPage({ events }: DjEvents) {
   return (
     <Layout>
-      <h1>Events Page</h1>
+      <h1>Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
       {events.map((evt: DjEvent, index: number) => (
         <EventItem key={evt.id} evt={evt} index={index} />
       ))}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: { events }
+  }
 }
