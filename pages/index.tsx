fdb217b462ba5408b0d808fe7be3419a108a4bc6
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2023-10-10 22:22:14 +0200

    Add view all events btn to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
+import Link from 'next/link'
 import { API_URL } from '@/config/index'
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
 import { DjEvents, DjEvent } from '@/types/dj-events'
-
-export async function getServerSideProps() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
-  return {
-    props: { events: events.slice(0, 3) }
-  }
-}
+import { URL_PATH_EVENTS } from '@/constants'
 
 export default function HomePage({ events }: DjEvents) {
   return (
@@ -21,6 +15,19 @@ export default function HomePage({ events }: DjEvents) {
           <EventItem key={evt.id} evt={evt} index={index} />
         ))}
       </div>
+      {events.length > 0 && (
+        <Link href={URL_PATH_EVENTS} className="btn-secondary">
+          View All Events
+        </Link>
+      )}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: { events: events.slice(0, 3) }
+  }
 }
