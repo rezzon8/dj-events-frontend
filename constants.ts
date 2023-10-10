ff94279d2540f4584c3abec1b3ec13e57cee2afc
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2023-10-10 22:18:40 +0200

    Add API paths to constants

--- constants.ts
+++ constants.ts
@@ -6,6 +6,9 @@ export const URL_PATH_HOME = '/'
 export const URL_PATH_ABOUT = '/about'
 export const URL_PATH_EVENTS = '/events'
 
+export const API_PATH_ROOT = '/api'
+export const API_PATH_EVENTS = `${API_PATH_ROOT}/events`
+
 export enum CONTACT_METHOD {
   EMAIL = 'EMAIL',
   MOBILE = 'MOBILE'
